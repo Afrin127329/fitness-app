e277140022304631ea2be2c8857ce9e9d4f103b1
Afrin Nahar <afrinnahar1999@gmail.com>
2023-09-04 20:40:47 +0600

    Updated our class page

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,6 +1,19 @@
 export enum SelectedPage {
-    Home = 'home',
-    Benefits = 'benefits',
-    OurClasses = 'ourClasses',
-    ContactUs = 'contactUs'
-}
+    Home = "home",
+    Benefits = "benefits",
+    OurClasses = "ourclasses",
+    ContactUs = "contactus",
+  }
+  
+  export interface BenefitType {
+    icon: JSX.Element;
+    title: string;
+    description: string;
+  }
+  
+  export interface ClassType {
+    name: string;
+    description?: string;
+    image: string;
+  }
+  
