ae101d48e6e77dfa7ecdef743633c7eae4f72534
Afrin Nahar <afrinnahar1999@gmail.com>
2023-09-05 12:51:37 +0600

    updated configuration

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -74,7 +74,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
 
       {/* MOBILE MENU MODAL */}
       {!isAboveMediumScreens && isMenuToggled && (
-        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+        <div className="fixed bottom-0 right-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
           {/* CLOSE ICON */}
           <div className="flex justify-end p-12">
             <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
